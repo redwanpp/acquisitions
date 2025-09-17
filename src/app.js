1c0f2aecef874ec659a59ed83b7ba09e96d08115
redwanpp <smredwan.info@gmail.com>
2025-09-17 12:57:21 +0600

    setup logging and middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import logger from '#config/logger.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisition!');
   res.status(200).send('Hello from Acquisitions API!');
 });
 
